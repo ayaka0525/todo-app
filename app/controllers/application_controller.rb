0ea43d687ad7e91e54e8d2b2bcdbb53fc3612dc5
eimii@0077 <yuimw03260323@yahoo.co.jp>
2024-09-04 16:16:15 +0900

    デザイン付ける

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :authenticate_user!
+
+  def authenticate_user!
+    redirect_to new_user_session_path unless user_signed_in?
+  end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,6 @@
 # Pin npm packages by running ./bin/importmap
-
-pin "application"
+# config/importmap.rb
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "@hotwired--turbo-rails.js" # @8.0.5
+pin "@hotwired/turbo", to: "@hotwired--turbo.js" # @8.0.5
+pin "@rails/actioncable/src", to: "@rails--actioncable--src.js" # @7.2.100
