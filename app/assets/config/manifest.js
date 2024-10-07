bba337a0b5cb741a46d23b1d51b0134c4df613e3
eimii@0077 <yuimw03260323@yahoo.co.jp>
2024-10-07 15:14:35 +0900

    ボタンを横並びにする

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,7 @@
+// app/assets/config/manifest.js
+
 //= link_tree ../images
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
-// app/assets/config/manifest.js
 //= link application.bootstrap.scss
